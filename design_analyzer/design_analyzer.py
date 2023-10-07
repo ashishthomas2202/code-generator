2247a110ab2bcdfdcdfc407cb57533cde9ca02d5
Ashish Thomas <ashishthomas2202@gmail.com>
2023-10-07 19:30:02 -0400

    refined open ai api code

--- design_analyzer/design_analyzer.py
+++ design_analyzer/design_analyzer.py
@@ -40,26 +40,46 @@ def analyze(project):
     prompt = f"""Generate style guide dictionaries based on the brand's identity: {project}. Make decisions based on the look and feel mentioned in the requirements. Like if the brand is luxurious, the style guide should be minimalistic and luxurious.
     or if the brand is modern, the style guide should be modern and minimalistic. same with colors
     Consider aspects such as Color Palette, Padding, Gutter, Layout Style, Design Style, and Typography for each style guide.
-    output must be in python dictionary format like {output_format} and it must be very detailed. output must not have any descriptive text before the dictionary.
+    output must be in python dictionary format like {output_format} and it must be very detailed. output must not have any descriptive text before the dictionary. The style guide must be based on professional design principles and professionally used font names and color codes.
+    the output must have the root key as 'design' make sure the output will contain the values (with units applied)
 """
 
     response = createResponse(prompt)
 
-    design = ast.literal_eval(response)
+    design = ast.literal_eval(response)['design']
 
     return design
 
 
 def createResponse(prompt):
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=1,  # Adjust temperature for desired creativity
-        max_tokens=2000,    # Adjust max_tokens based on desired response length
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
+    # response = openai.Completion.create(
+    #     # model="text-davinci-003",
+    #     model="gpt-3.5-turbo-instruct",
+    #     prompt=prompt,
+    #     temperature=1,  # Adjust temperature for desired creativity
+    #     max_tokens=3800,    # Adjust max_tokens based on desired response length
+    #     top_p=1.0,
+    #     frequency_penalty=0.0,
+    #     presence_penalty=0.0
+    # )
+
+    # return response.choices[0].text
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=1,
+        max_tokens=10000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
 
-    return response.choices[0].text
+    print(response)
+    return response.choices[0].message.content
--- requirement_extractor/requirement_extractor.py
+++ requirement_extractor/requirement_extractor.py
@@ -30,25 +30,6 @@ def initiate():
         if len(project["description"]) > 5:
             done = True
 
-    # done = False
-    # while not done:
-    #     project["client_info"]['name'] = input("Enter Client's Name: ")
-    #     if len(project["client_info"]['name']) > 5:
-    #         done = True
-
-    # done = False
-    # while not done:
-    #     project["client_info"]['email'] = input("Enter Client's Email: ")
-    #     if len(project["client_info"]['email']) > 5:
-    #         done = True
-
-    # done = False
-    # while not done:
-    #     project["client_info"]['phone'] = input(
-    #         "Enter Client's Phone Number: ")
-    #     if len(project["client_info"]['phone']) > 5:
-    #         done = True
-
     done = False
     while not done:
         target_platforms = input(
@@ -115,18 +96,6 @@ def initiate():
         if len(project["requirements"]["description"]) > 5:
             done = True
 
-    # done = False
-    # while not done:
-    #     project["deadline"] = input("Enter project deadline: ")
-    #     if len(project["deadline"]) > 5:
-    #         done = True
-
-    # done = False
-    # while not done:
-    #     project["budget"] = input("Enter project budget: ")
-    #     if len(project["budget"]) > 1:
-    #         done = True
-
     done = False
     while not done:
         project["additional_notes"] = input("Enter additional notes: ")
@@ -134,93 +103,3 @@ def initiate():
             done = True
 
     return project
-
-    # for key, value in fields.items():
-    #     if isinstance(value, dict):
-    #         print(key)
-    #         for subkey, subvalue in value.items():
-    #             print(subkey)
-    #     else:
-    #         print(key, value)
-
-    # done = True
-#             # Check if the field is missing in the requirements dictionary
-#             if field not in requirements:
-#                 # Prompt the user for the missing information
-#                 user_input = input(f"Please provide {field}: ")
-
-#                 # Update the requirements dictionary with user input
-#                 requirements[field] = user_input
-
-#                 # Mark the field as answered
-#                 answered_fields.add(field)
-
-#     # Ask if backend is needed
-#     while True:
-#         backend_required = input("Is a backend required? (yes/no): ").lower()
-#         if backend_required in ["yes", "no"]:
-#             requirements["requirements"] = {
-#                 "backend_required": (backend_required == 'yes')
-#             }
-#             break
-#         else:
-#             print("Please enter 'yes' or 'no'.")
-
-#     # Ask for target platforms
-#     while True:
-#         target_platforms = input(
-#             "Enter targeted platform(s) (comma-separated, e.g., Web, Mobile, Desktop): ")
-#         platforms = [platform.strip()
-#                      for platform in target_platforms.split(",")]
-#         valid_platforms = ["Web", "Mobile", "Desktop"]
-
-#         if all(platform in valid_platforms for platform in platforms):
-#             requirements["requirements"]["target_platforms"] = platforms
-#             break
-#         else:
-#             print(
-#                 "Invalid platform(s). Please use 'Web', 'Mobile', 'Desktop' or combinations.")
-
-#     # Once all required fields are filled, return the complete requirements
-#     return requirements
-
-
-# # # Example usage
-# # if __name__ == "__main__":
-# #     software_requirements = analyze()
-# #     print("Software Requirements:")
-# #     print(json.dumps(software_requirements, indent=2))
-
-# #
-# # import os
-# # import openai
-# # import json
-
-
-# # def analyze(request):
-# #     print("analyze")
-
-# #     openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# #     # Updated prompt with more specific instructions
-# #     prompt = f"Translate the user request \"{request}\" into structured software development requirements in JSON format. Include project_name, project_description, client_info, requirements, deadline, budget, and any additional_notes. You may also ask for any missing details."
-
-# #     response = openai.Completion.create(
-# #         model="text-davinci-003",
-# #         prompt=prompt,
-# #         temperature=0.7,  # Adjust temperature for desired creativity
-# #         max_tokens=300,    # Adjust max_tokens based on desired response length
-# #         top_p=1.0,
-# #         frequency_penalty=0.0,
-# #         presence_penalty=0.0
-# #     )
-
-# #     # try:
-# #     #     # Attempt to parse the AI response as JSON
-# #     #     print(f"response: {response}")
-# #     #     parsed_response = json.loads(response.choices[0].text)
-# #     # except json.JSONDecodeError as e:
-# #     #     parsed_response = {"error": "Failed to parse AI response as JSON",
-# #     #                        "raw_response": response.choices[0].text}
-
-# #     return response.choices[0].text
